Silverlan/panima

commit 2a9be5bc5bb775105ef05230c5d05900b1efe7d7

Add ChannelPath URI structure

include/panima/channel.hpp
@@ -29,6 +29,17 @@ namespace panima
 		float duration = -1.f;
 	};
 
+	// Example URI: panima:ec/color/color?components=red,blue
+	struct ChannelPath
+	{
+		ChannelPath()=default;
+		ChannelPath(const std::string &path);
+		std::string path;
+		std::optional<std::vector<std::string>> components;
+
+		std::string ToUri() const;
+	};
+
 	namespace expression {struct ValueExpression;};
 	struct Channel
 		: public std::enable_shared_from_this<Channel>

src/channel.cpp
@@ -8,8 +8,55 @@
 #include "panima/channel.hpp"
 #include "value_expression.hpp"
 #include <udm.hpp>
+#include <sharedutils/util_uri.hpp>
 
 static constexpr auto VALUE_EPSILON = 0.001f;
+
+panima::ChannelPath::ChannelPath(const std::string &path)
+{
+	uriparser::Uri uri {path};
+	auto scheme = uri.scheme();
+	if(!scheme.empty() && scheme != "panima")
+		return; // Invalid panima URI
+	ChannelPath channelPath;
+	channelPath.path = uri.path();
+	auto strQueries = uri.query();
+	std::vector<std::string> queries;
+	ustring::explode(strQueries,"&",queries);
+	for(auto &queryParam : queries)
+	{
+		std::vector<std::string> query;
+		ustring::explode(queryParam,"=",query);
+		if(query.size() < 2)
+			continue;
+		if(query.front() == "components")
+		{
+			channelPath.components = std::vector<std::string>{};
+			ustring::explode(query[1],",",*channelPath.components);
+		}
+	}
+}
+std::string panima::ChannelPath::ToUri() const
+{
+	std::string uri = "panima:" +path;
+	if(components.has_value())
+	{
+		std::string strComponents;
+		for(auto first=true;auto &c : *components)
+		{
+			if(first)
+				first = false;
+			else
+				strComponents += ",";
+			strComponents += c;
+		}
+		if(!strComponents.empty())
+			uri += "?" +strComponents;
+	}
+	return uri;
+}
+
+////////////////
 
 panima::Channel::Channel()
 	: m_times{::udm::Property::Create(udm::Type::ArrayLz4)},m_values{::udm::Property::Create(udm::Type::ArrayLz4)}

commit de1fa2dc5fdf0e1310da05c38d86048f73528d37

Add ostream << overload for TimeFrame and ChannelPath; Fix issues introduced in 2a9be5b

include/panima/animation.hpp
@@ -52,6 +52,9 @@ namespace panima
 
 		float GetDuration() const {return m_duration;}
 		void SetDuration(float duration) {m_duration = duration;}
+
+		bool operator==(const Animation &other) const {return this == &other;}
+		bool operator!=(const Animation &other) const {return !operator==(other);}
 	private:
 		std::vector<std::shared_ptr<Channel>> m_channels;
 		std::string m_name;

include/panima/animation_manager.hpp
@@ -71,6 +71,9 @@ namespace panima
 		const panima::AnimationSet *FindAnimationSet(const std::string &name) const {return const_cast<AnimationManager*>(this)->FindAnimationSet(name);}
 
 		void SetCallbackInterface(const AnimationPlayerCallbackInterface &i) {m_callbackInterface = i;}
+
+		bool operator==(const AnimationManager &other) const {return this == &other;}
+		bool operator!=(const AnimationManager &other) const {return !operator==(other);}
 	private:
 		std::optional<AnimationSetIndex> FindAnimationSetIndex(const std::string &name) const;
 		AnimationReference FindAnimation(AnimationSetIndex animSetIndex,panima::AnimationId animation,PlaybackFlags flags) const;

include/panima/animation_set.hpp
@@ -41,6 +41,9 @@ namespace panima
 
 		void Reserve(uint32_t count);
 		uint32_t GetSize() const;
+
+		bool operator==(const AnimationSet &other) const {return this == &other;}
+		bool operator!=(const AnimationSet &other) const {return !operator==(other);}
 	private:
 		AnimationSet();
 		std::vector<std::shared_ptr<Animation>> m_animations;

include/panima/channel.hpp
@@ -37,7 +37,7 @@ namespace panima
 		std::string path;
 		std::optional<std::vector<std::string>> components;
 
-		std::string ToUri() const;
+		std::string ToUri(bool includeScheme=true) const;
 	};
 
 	namespace expression {struct ValueExpression;};
@@ -146,6 +146,9 @@ namespace panima
 
 		void Resize(uint32_t numValues);
 		uint32_t GetSize() const;
+
+		bool operator==(const Channel &other) const {return this == &other;}
+		bool operator!=(const Channel &other) const {return !operator==(other);}
 	private:
 		void TimeToLocalTimeFrame(float &inOutT) const;
 		template<typename T>
@@ -160,5 +163,8 @@ namespace panima
 };
 
 std::ostream &operator<<(std::ostream &out,const panima::Channel &o);
+std::ostream &operator<<(std::ostream &out,const panima::TimeFrame &o);
+std::ostream &operator<<(std::ostream &out,const panima::ChannelPath &o);
+
 
 #endif

include/panima/player.hpp
@@ -60,6 +60,9 @@ namespace panima
 
 		Player &operator=(const Player &other);
 		Player &operator=(Player &&other);
+
+		bool operator==(const Player &other) const {return this == &other;}
+		bool operator!=(const Player &other) const {return !operator==(other);}
 	private:
 		Player();
 		Player(const Player &other);

src/channel.cpp
@@ -12,14 +12,15 @@
 
 static constexpr auto VALUE_EPSILON = 0.001f;
 
-panima::ChannelPath::ChannelPath(const std::string &path)
+panima::ChannelPath::ChannelPath(const std::string &ppath)
 {
-	uriparser::Uri uri {path};
+	uriparser::Uri uri {ppath};
 	auto scheme = uri.scheme();
 	if(!scheme.empty() && scheme != "panima")
 		return; // Invalid panima URI
-	ChannelPath channelPath;
-	channelPath.path = uri.path();
+	path = uri.path();
+	if(!path.empty() && path.front() == '/')
+		path.erase(path.begin());
 	auto strQueries = uri.query();
 	std::vector<std::string> queries;
 	ustring::explode(strQueries,"&",queries);
@@ -31,14 +32,17 @@ panima::ChannelPath::ChannelPath(const std::string &path)
 			continue;
 		if(query.front() == "components")
 		{
-			channelPath.components = std::vector<std::string>{};
-			ustring::explode(query[1],",",*channelPath.components);
+			components = std::vector<std::string>{};
+			ustring::explode(query[1],",",*components);
 		}
 	}
 }
-std::string panima::ChannelPath::ToUri() const
+std::string panima::ChannelPath::ToUri(bool includeScheme) const
 {
-	std::string uri = "panima:" +path;
+	std::string uri;
+	if(includeScheme)
+		uri = "panima:";
+	uri += path;
 	if(components.has_value())
 	{
 		std::string strComponents;
@@ -344,5 +348,19 @@ std::ostream &operator<<(std::ostream &out,const panima::Channel &o)
 	auto n = o.GetTimeCount();
 	if(n > 0)
 		out<<"[TimeRange:"<<*o.GetTime(0)<<","<<*o.GetTime(n -1)<<"]";
+	return out;
+}
+std::ostream &operator<<(std::ostream &out,const panima::TimeFrame &o)
+{
+	out<<"TimeFrame";
+	out<<"[StartOffset:"<<o.startOffset<<"]";
+	out<<"[Scale:"<<o.scale<<"]";
+	out<<"[Duration:"<<o.duration<<"]";
+	return out;
+}
+std::ostream &operator<<(std::ostream &out,const panima::ChannelPath &o)
+{
+	out<<"ChannelPath";
+	out<<"["<<o.ToUri()<<"]";
 	return out;
 }